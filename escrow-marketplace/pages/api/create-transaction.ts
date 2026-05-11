f8b87493dda3e355dde96709459645debfe2a74d
StackwellDigital <adam@stackwell.digital>
2026-05-10 17:12:25 -0700

    Update create-transaction.ts

--- escrow-marketplace/pages/api/create-transaction.ts
+++ escrow-marketplace/pages/api/create-transaction.ts
@@ -34,7 +34,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const paymentIntent = await stripe.paymentIntents.create({
       amount: depositAmount,
       currency: 'usd',
-      metadata: { itemName, sellerEmail: user.email },
+      metadata: { itemName, sellerEmail: user.email ?? '' },
     })
 
     // Create transaction row
